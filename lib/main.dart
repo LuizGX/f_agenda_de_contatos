b4ced15062d4c8a851cae815c126703ae1efbcf5
Luiz Gustavo Xavier <luizxvr@hotmail.com>
2019-12-09 12:38:40 -0300

    telas, edicao, exclusao

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:f_agenda_de_contatos/UI/home_page.dart';
 import 'package:flutter/material.dart';
 
+import 'UI/contact_page.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
